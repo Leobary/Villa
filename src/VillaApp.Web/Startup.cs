c65f77431c4c3645edb456312fed3ce187a9516d
Leobary <leobary86@yahoo.com>
2020-05-29 11:34:55 +0100

    added database connection to startup.cs

--- src/VillaApp.Web/Startup.cs
+++ src/VillaApp.Web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VillaApp.DatabaseContexts.AuthenticationDbContext;
 
 namespace VillaApp.Web
 {
@@ -20,9 +22,11 @@ namespace VillaApp.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection"))); 
             services.AddControllersWithViews();
         }
 
