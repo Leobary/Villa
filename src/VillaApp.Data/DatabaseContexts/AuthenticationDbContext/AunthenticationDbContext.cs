c2e17471aeb7b87e2c0af97a5500f92415a8fed1
Leobary <leobary86@yahoo.com>
2020-05-29 10:29:03 +0100

    added aunthentication db context class

--- src/VillaApp.Data/DatabaseContexts/AuthenticationDbContext/AunthenticationDbContext.cs
+++ src/VillaApp.Data/DatabaseContexts/AuthenticationDbContext/AunthenticationDbContext.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VillaApp.Data.Entities;
+
 namespace VillaApp.DatabaseContexts.AuthenticationDbContext
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+            : base(options)
+            {
+
+            }
     }
 }
---
+++ src/VillaApp.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace VillaApp.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
