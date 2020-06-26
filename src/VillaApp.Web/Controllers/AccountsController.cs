82e8049e24ddf5bc90797dca809944480d62dd91
Leobary <leobary86@yahoo.com>
2020-06-26 22:45:15 +0100

    added models for login/register

--- src/VillaApp.Web/Controllers/AccountsController.cs
+++ src/VillaApp.Web/Controllers/AccountsController.cs
@@ -1,17 +1,35 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using VillaApp.Web.Models;
 
 namespace VillaApp.Web.Controllers
 {
     public class AccountsController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if(!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if(!ModelState.IsValid) return View();
+            throw new NotImplementedException();
         }
     }
 }
